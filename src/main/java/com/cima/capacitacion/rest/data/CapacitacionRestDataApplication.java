7176e02fba37445e605d3f6f214a158215a310f1
maurodanielpenaloza <maurodanielpenaloza@gmail.com>
2018-08-17 12:20:00 -0300

    Esqueleto de proyecto maven

---
+++ src/main/java/com/cima/capacitacion/rest/data/CapacitacionRestDataApplication.java
@@ -0,0 +1,12 @@
+package com.cima.capacitacion.rest.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapacitacionRestDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapacitacionRestDataApplication.class, args);
+    }
+}
---
+++ src/test/java/com/cima/capacitacion/rest/data/CapacitacionRestDataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cima.capacitacion.rest.data;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CapacitacionRestDataApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
